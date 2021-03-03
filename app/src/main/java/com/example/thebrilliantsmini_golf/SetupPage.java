943a9ed3956b7ee43b20dc0bd5a81491b95e7b2e
Justin Nelson <JDNelson109@gmail.com>
2021-03-03 12:05:12 -0500

    Added button to go to game setup page. Also added the set up page.

---
+++ app/src/main/java/com/example/thebrilliantsmini_golf/SetupPage.java
@@ -0,0 +1,12 @@
+package com.example.thebrilliantsmini_golf;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class SetupPage extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_setup);
+    }
+}
