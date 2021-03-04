c746493f3680b6fa8fe848536d31afe9e1b4c03f
Avinash117 <thomasj29@vcu.edu>
2021-03-04 15:02:22 -0500

    Added button functionality to most activities( disregard last statement from previous commit )

--- app/src/main/java/com/example/thebrilliantsmini_golf/newGameActivity.java
+++ app/src/main/java/com/example/thebrilliantsmini_golf/newGameActivity.java
@@ -13,6 +13,20 @@ public class newGameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_game);
+
+        Button play = findViewById(R.id.letsPlayButton);
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                letsPlay();
+            }
+        });
+
+    }
+
+    void letsPlay(){
+        Intent intent = new Intent(this, scoreboardActivity.class);
+        startActivity(intent);
     }
 
 }
