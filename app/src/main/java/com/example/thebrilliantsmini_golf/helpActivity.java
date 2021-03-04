ce489bb669ae3fae7270bf4709f5b9eda090f34e
Avinash117 <thomasj29@vcu.edu>
2021-03-04 14:56:28 -0500

    Finished activities

---
+++ app/src/main/java/com/example/thebrilliantsmini_golf/helpActivity.java
@@ -0,0 +1,14 @@
+package com.example.thebrilliantsmini_golf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class helpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.help_page);
+    }
+}
