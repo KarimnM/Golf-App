083d9c4784f1bac0e2ccaabf4a29c15ac59ebc12
KarimnM <mahmoudkn@mymail.vcu.edu>
2021-03-05 18:59:20 -0500

    Splash Screen is created.

---
+++ app/src/main/java/com/example/thebtilliantsmini_golf/Splash.java
@@ -0,0 +1,34 @@
+package com.example.thebtilliantsmini_golf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                try{
+                    sleep(3000);
+                    Intent i = new Intent(Splash.this, MainActivity.class);
+                    startActivity(i);
+                }catch (Exception e){
+                    Log.e("Splash error", e.getMessage());
+                }
+            }
+        }.start();
+
+
+
+    }
+}
