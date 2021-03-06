a390adffaeb0fced6c2fed9da9e6091ff732bef8
desaisb3 <desaisb3@vcu.edu>
2021-03-05 21:14:46 -0500

    okay bye

---
+++ app/src/main/java/com/example/thebtilliantsmini_golf/NewgameActivity.java
@@ -0,0 +1,44 @@
+package com.example.thebtilliantsmini_golf;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewgameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newgame);
+        Button play = findViewById(R.id.letsPlayButton);
+        ImageButton hpImBt3 = findViewById(R.id.HPimageBt3);
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                letsPlay();
+            }
+        });
+
+        hpImBt3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainActivity();
+            }
+        });
+
+    }
+
+    void letsPlay(){
+        Intent intent = new Intent(this, ScoreboardActivity.class);
+        startActivity(intent);
+    }
+
+    void mainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
