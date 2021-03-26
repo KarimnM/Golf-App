f6f2cd7211dea6c1d42c61e50fafafe93ec6a751
Justin Nelson <JDNelson109@gmail.com>
2021-03-25 21:17:53 -0400

    Fixed error causing build to fail.

--- app/src/main/java/com/example/thebtilliantsmini_golf/Player.java
+++ app/src/main/java/com/example/thebtilliantsmini_golf/Player.java
@@ -6,26 +6,30 @@ public class Player {
     int score;
 
     //Constructor
-    public Player(String name, int score){
-        name=name; //To be given on creation
+    public Player(String playerName, int score){
+        name=playerName; //To be given on creation
         score = 0; //Score starts at 0
     }
 
     //Set player score
     public void setScore(int newScore){
+
         score = newScore;
     }
     //Set player name
     public void setName(String newName){
+
         name = newName;
     }
 
     //Return score
     public int getScore(){
+
         return score;
     }
     //Return name
     public String getName() {
+
         return name;
     }
 }
