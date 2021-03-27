0a6b3f68b3c449e4306af98405c0275c1d5e2ce0
chungrs <54409983+chungrs@users.noreply.github.com>
2021-03-26 23:46:35 -0400

    edited Player class - created enum for ball color (red, blue, yellow, green, purple, orange) and an array that allows player's score for each hole to be stored, and appropriate methods that get and set score for each hole as well as getter and setter for total score

--- app/src/main/java/com/example/thebtilliantsmini_golf/Player.java
+++ app/src/main/java/com/example/thebtilliantsmini_golf/Player.java
@@ -2,34 +2,63 @@ package com.example.thebtilliantsmini_golf;
 
 public class Player {
 
+    public enum Color {
+        RED, BLUE, YELLOW, GREEN, PURPLE, ORANGE;
+    }
+
     String name;
-    int score;
+    int totalScore;
+    Color color;
+    int score[];
 
     //Constructor
-    public Player(String playerName, int score){
-        name=playerName; //To be given on creation
-        score = 0; //Score starts at 0
+    public Player(String playerName, Color color){
+        name = playerName; //To be given on creation
+        totalScore = 0; //Score starts at 0
+        this.color = color;
+        score = new int[18]; //create array for score of each hole
     }
 
     //Set player score
-    public void setScore(int newScore){
-
-        score = newScore;
+    public void setTotalScore() {
+        for(int i = 0; i < score.length; i++) {
+            totalScore += score[i];
+        };
     }
+
+    //Set score for hole
+    public void setHoleScore(int hole, int score) {
+        this.score[hole-1] = score; //hole-1 because hole 1 is element 0 in array
+    }
+
+    //Set ball color
+    public void setColor(Color color){
+        this.color = color;
+    }
+
     //Set player name
-    public void setName(String newName){
-
-        name = newName;
+    public void setName(String name){
+        this.name = name;
     }
 
-    //Return score
-    public int getScore(){
-
-        return score;
+    //Return score for hole
+    public int getCurrentHoleScore(int hole) {
+        return score[hole - 1];
     }
+
+    //Get Color
+    public String getColor() {
+        return color.toString();
+    }
+
+    //Return total score
+    public int getTotalScore() {
+        return totalScore;
+    }
+
     //Return name
     public String getName() {
-
         return name;
     }
+
 }
