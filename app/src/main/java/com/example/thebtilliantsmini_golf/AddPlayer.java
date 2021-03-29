4b061c57e9181439250bfdf1d747be6ea11eadbd
chungrs <54409983+chungrs@users.noreply.github.com>
2021-03-29 01:12:40 -0400

    testing edits

--- app/src/main/java/com/example/thebtilliantsmini_golf/AddPlayer.java
+++ app/src/main/java/com/example/thebtilliantsmini_golf/AddPlayer.java
@@ -31,66 +31,59 @@ public class AddPlayer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_player2);
-            getPlayerName = (EditText) findViewById(R.id.getPlayerName);
-            getRecipeName = (EditText) findViewById(R.id.getRecipeName);
-            Adding = (Button) findViewById(R.id.Add);
-            save = (Button) findViewById(R.id.Save);
-            addedItems = (ListView) findViewById(R.id.VV);
-            DB = new DBManger(AddPlayer.this);
-            IngredientsNames = new ArrayList<>();
-            //    mDatabaseHelp = new DatabaseHelper()
+        getPlayerName = (EditText) findViewById(R.id.getPlayerName);
+        getRecipeName = (EditText) findViewById(R.id.getRecipeName);
+        Adding = (Button) findViewById(R.id.Add);
+        save = (Button) findViewById(R.id.Save);
+        addedItems = (ListView) findViewById(R.id.VV);
+        DB = new DBManger(AddPlayer.this);
+        IngredientsNames = new ArrayList<>();
+        //    mDatabaseHelp = new DatabaseHelper()
 
 
-            arrayList = new ArrayList<Player>();
-            adapter = new ArrayAdapter<String>(AddPlayer.this, android.R.layout.simple_list_item_1, IngredientsNames);
+        arrayList = new ArrayList<Player>();
+        adapter = new ArrayAdapter<String>(AddPlayer.this, android.R.layout.simple_list_item_1, IngredientsNames);
 
-            addedItems.setAdapter(adapter);
-            onBtnClick();
-        }
+        addedItems.setAdapter(adapter);
+        onBtnClick();
+    }
 
-        public void onBtnClick() {
-            Adding.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    //Ingredients
-                  if(arrayList.size()==4){//allows a maximum of 4 players
-                      Toast.makeText(AddPlayer.this, "Maximum of 4 players", Toast.LENGTH_SHORT).show();
-                  }else if(arrayList.size()<4) {
+    public void onBtnClick() {
+        Adding.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Ingredients
+                if(arrayList.size()==4){//allows a maximum of 4 players
+                    Toast.makeText(AddPlayer.this, "Maximum of 4 players", Toast.LENGTH_SHORT).show();
+                }else if(arrayList.size()<4) {
 
-                      Player I = new Player();
-                      String finaly = getPlayerName.getText().toString();
-                      I.setContent(finaly);
-                      getPlayerName.setText("");
-                      arrayList.add(I);
-                      IngredientsNames.add(finaly);
-                      adapter.notifyDataSetChanged();
-                  }
+                    Player I = new Player();
+                    String finaly = getPlayerName.getText().toString();
+                    I.setContent(finaly);
+                    getPlayerName.setText("");
+                    arrayList.add(I);
+                    IngredientsNames.add(finaly);
+                    adapter.notifyDataSetChanged();
                 }
-            });
+            }
+        });
 
-            save.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
                 //   Recipe r = new Recipe();
-                  //  r.setName(getRecipeName.getText().toString());
-                   // r.setIngredientsList(arrayList);
-                   // r.setLastUpdate(Calendar.getInstance().getTimeInMillis());
-                   // DB.insertRecpiToDataBase(r);
-                 //   Intent i = new Intent(getApplicationContext(), ShowAllRecipes.class);
-                  //  startActivity(i);
+                //  r.setName(getRecipeName.getText().toString());
+                // r.setIngredientsList(arrayList);
+                // r.setLastUpdate(Calendar.getInstance().getTimeInMillis());
+                // DB.insertRecpiToDataBase(r);
+                Intent i = new Intent(getApplicationContext(), ScoreboardActivity.class);
+                startActivity(i);
 
-               }
-           });
-
-
-        }
-
-      //  public void Bakbtnn(View view) {
-        //    Intent itent = new Intent(this, ShowAllRecipes.class);
-         //   startActivity(itent);
-        }
+            }
+        });
 
 
+    }
 
-
-
+}
