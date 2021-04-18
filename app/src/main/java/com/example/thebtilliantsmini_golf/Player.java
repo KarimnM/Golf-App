7557225e317be5f461ec3ac0b9fa7077a68814fa
Justin Nelson <JDNelson109@gmail.com>
2021-04-18 00:24:44 -0400

    Added constructor for the player class

--- app/src/main/java/com/example/thebtilliantsmini_golf/Player.java
+++ app/src/main/java/com/example/thebtilliantsmini_golf/Player.java
@@ -1,6 +1,7 @@
 package com.example.thebtilliantsmini_golf;
 
 import java.io.Serializable;
+import java.util.HashMap;
 
 public class Player implements Serializable {
     private String content;
@@ -8,6 +9,22 @@ public class Player implements Serializable {
     String color;
     int score;
     int colorListPosition;
+
+    public Player() {
+        for(int i=1; i<10;i++){
+            scoreValues.put(i+"",-1);
+        }
+    }
+
+    public HashMap<String, Integer> getScoreValues() {
+        return scoreValues;
+    }
+
+    public void setScoreValues(HashMap<String, Integer> scoreValues) {
+        this.scoreValues = scoreValues;
+    }
+
+    private HashMap<String ,Integer> scoreValues=new HashMap<>();
 
     public int getColorListPosition() {
         return colorListPosition;
