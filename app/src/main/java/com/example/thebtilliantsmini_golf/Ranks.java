db9a2dc889075628e0c89807593613477d10be92
KarimnM <mahmoudkn@mymail.vcu.edu>
2021-04-30 00:30:28 -0400

    Ranks layout

---
+++ app/src/main/java/com/example/thebtilliantsmini_golf/Ranks.java
@@ -0,0 +1,14 @@
+package com.example.thebtilliantsmini_golf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ranks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ranks);
+    }
+}
