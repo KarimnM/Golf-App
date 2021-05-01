d554db6b68d1d65811f5bed5b258add763ca8fba
desaisb3 <desaisb3@vcu.edu>
2021-05-01 00:59:49 -0400

    removed old new game activity

--- app/src/main/java/com/example/thebtilliantsmini_golf/NewGameActivity.java
+++ app/src/main/java/com/example/thebtilliantsmini_golf/NewGameActivity.java
@@ -14,22 +14,6 @@ public class NewGameActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_newgame);
-        Button play = findViewById(R.id.letsPlayButton);
-        ImageButton hpImBt3 = findViewById(R.id.HPimageBt3);
-        play.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                letsPlay();
-            }
-        });
-
-        hpImBt3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mainActivity();
-            }
-        });
 
     }
 
